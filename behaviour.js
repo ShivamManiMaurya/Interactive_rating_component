d43e09d124d15e3d9c4584c42ffae692d118931c
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-02-14 21:10:04 +0530

    Update behaviour.js

--- behaviour.js
+++ behaviour.js
@@ -1,36 +1,20 @@
-// alert("hellow dunia");
 
 var ratingVal = "";
-
-function passingValues() {
-    localStorage.setItem("textvalue", ratingVal);
-    return false;
-}
-
 
 $(document).ready(function(){
     $(".btn").on("click", function(){
         ratingVal = $(this).text();
-
-        
     });
 
-    $(".submit-btn").click(function(){
+    $("#btn-ratings").submit(function (event){
+        event.preventDefault();
 
-        // window.addEventListener("load", function() {
-            
-        //     this.document.getElementById("span-id").innerHTML = ratingVal;
-
-        // })
-
-                // window.open('resultpage.html');
-        
-
-        // passingValues();
-
-        window.location.href = "resultpage.html?name=" + encodeURIComponent($(".btn").val());
-        
-        
+        if (ratingVal > 0)
+        {
+            $(".rating").css("display", "none");
+            $(".thanks").css("display", "grid");
+            $("span").text(ratingVal);
+        }
     });
     
 });
